0f971298d6aaf0e2e63d45962e912383a6f3b8a5
thehanemperor <sh442555789@gmail.com>
2020-12-03 00:42:02 -0800

    change

--- script.sh
+++ script.sh
@@ -2,24 +2,24 @@
  
  for i in {1..5} 
  do 
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 10 >> testResult/threads/workload1/rocksDB1000-threads10.txt 
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 10 >> testResult/threads/workload1/rocksDB100000-threads10.txt 
  done
  for i in {1..5}
  do 
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 20 >> testResult/threads/workload1/rocksDB1000-threads20.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 20 >> testResult/threads/workload1/rocksDB100000-threads20.txt
  done
  
  for i in {1..5}
  do
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 40 >> testResult/threads/workload1/rocksDB1000-threads40.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 40 >> testResult/threads/workload1/rocksDB100000-threads40.txt
  done 
 
  for i in {1..5}
  do
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 80 >> testResult/threads/workload1/rocksDB1000-threads40.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 80 >> testResult/threads/workload1/rocksDB100000-threads80.txt
  done 
 
  for i in {1..5}
  do 
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 100 >> testResult/threads/workload1/rocksDB1000-threads40.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 100 >> testResult/threads/workload1/rocksDB100000-threads100.txt
 done 
