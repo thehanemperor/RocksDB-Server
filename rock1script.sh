884255f5c839214b4b8d3465dc6d7fa82e5e6f08
hs267 <hs267@mail.missouri.edu>
2020-12-03 01:35:53 -0800

    change to workload 2

--- rock1script.sh
+++ rock1script.sh
@@ -1,31 +1,31 @@
  #!/bin/bash
  
- for i in 1 2 3 4 
+ for i in 1 2 3 4 5
  do 
  echo "threads 10 $i"
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 10 >> testResult/threads/workload1/rocksDB100000-threads10.txt 
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 10 >> testResult/threads/workload2/rocksDB100000-threads10.txt 
  done
 
- for i in 1 2 3 4 
+ for i in 1 2 3 4 5
  do 
  echo "threads 20 $i"
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 20 >> testResult/threads/workload1/rocksDB100000-threads20.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 20 >> testResult/threads/workload2/rocksDB100000-threads20.txt
  done
  
- for i in 1 2 3 4 
+ for i in 1 2 3 4 5
  do
  echo "threads 40 $i"
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 40 >> testResult/threads/workload1/rocksDB100000-threads40.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 40 >> testResult/threads/workload2/rocksDB100000-threads40.txt
  done 
 
- for i in 1 2 3 4 
+ for i in 1 2 3 4 5
  do
  echo "threads 80 $i"
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 80 >> testResult/threads/workload1/rocksDB100000-threads80.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 80 >> testResult/threads/workload2/rocksDB100000-threads80.txt
  done 
 
- for i in 1 2 3 4 
+ for i in 1 2 3 4 5
  do 
  echo "threads 100 $i"
- ./bin/ycsb run rocksdbserver -s -P workloads/workloada -p operationcount=100000 -threads 100 >> testResult/threads/workload1/rocksDB100000-threads100.txt
+ ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 100 >> testResult/threads/workload2/rocksDB100000-threads100.txt
 done 
