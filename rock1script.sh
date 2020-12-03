fc5f52d8b19df9d4b9ddc767abf22244437ab3ee
hs267 <hs267@mail.missouri.edu>
2020-12-03 02:23:09 -0800

    Update rock1script.sh

--- rock1script.sh
+++ rock1script.sh
@@ -6,23 +6,35 @@
  ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 10 >> testResult/threads/workload2/rocksDB100000-threads10.txt 
  done
 
+echo "sleep..."
+sleep 3
+
  for i in 1 2 3 4 5
  do 
  echo "threads 20 $i"
  ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 20 >> testResult/threads/workload2/rocksDB100000-threads20.txt
  done
  
+ echo "sleep..."
+sleep 3
+
  for i in 1 2 3 4 5
  do
  echo "threads 40 $i"
  ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 40 >> testResult/threads/workload2/rocksDB100000-threads40.txt
  done 
 
+echo "sleep..."
+sleep 3
+
  for i in 1 2 3 4 5
  do
  echo "threads 80 $i"
  ./bin/ycsb run rocksdbserver -s -P workloads/workloadb -p operationcount=100000 -threads 80 >> testResult/threads/workload2/rocksDB100000-threads80.txt
  done 
+
+ echo "sleep..."
+sleep 3
 
  for i in 1 2 3 4 5
  do 
