9604035217fd2324526e429b396e0954bf3bbb7e
Han <sh442555789@gmail.com>
2020-12-02 23:05:20 -0800

    chagne bin

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -4,7 +4,7 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 # find the pid
-pid=$(pgrep -f "com.ranksays.rocksdb.server.Main")
+pid=$(pgrep -f "com.cs550.rocksdb.server.Main")
 
 if [ "$pid" != "" ];then
 	echo "Shutting down server (pid = $pid) .."
--- bin/startup.sh
+++ bin/startup.sh
@@ -4,4 +4,4 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 echo "Starting up server .."
-java -cp "./lib/*" com.ranksays.rocksdb.server.Main &
+java -cp "./lib/*" com.cs550.rocksdb.server.Main &
